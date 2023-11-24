67dbd851ef3fde0c3bf6a2b3211e12ac48518abd
Yeji Choi <cyei0824@gmail.com>
2023-11-24 19:56:40 +0900

    Initial commit

---
+++ src/main/java/com/example/wanso/WansoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wanso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WansoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WansoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wanso/WansoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wanso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WansoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
